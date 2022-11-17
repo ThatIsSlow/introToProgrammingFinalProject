11d0069ea349c60cb699a275ec7881552f7e4daf
ThatIsSlow <115042744+ThatIsSlow@users.noreply.github.com>
2022-11-17 11:22:14 -0800

    Reformatting + reorganizing

--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ PROJECT OUTLINE:
             - GAME DESIGN:
                 - most likely strategy based, maybe something like RISK or a towere defense game. Something Turn based would be cool.
                 - Definitely want to use mulitple levels
+
+                
         - HereNow (rowing results website) parser
             - Will likely use Tkinter
             - will enable the user to input a team name or a rower name or a boat class
@@ -25,37 +27,3 @@ PROJECT OUTLINE:
 Sources: 
 
 """
-
-import pygame as pg 
-from random import randint
-from pygame.sprite import Sprite
-from settings import *
-from sprites import *
-
-vec = pg.math.Vector2
-
-pg.init()
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption(" ")
-clock = pg.time.Clock()
-
-# Function to get things on screen
-mob1 = Mob(WHITE, 10)
-
-mobs = pg.sprite.Group()
-
-mobs.add(mob1)
-
-running = True
-while running:
-    delta = clock.tick(FPS)
-    for event in pg.event.get():
-        # check for closed window
-        if event.type == pg.QUIT:
-            running = False
-
-    mobs.draw(screen)
-    pg.display.flip()
-
-pg.quit()
