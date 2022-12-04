251bf9087c86a075e14dd7fffd5663db284728cb
ThatIsSlow <115042744+ThatIsSlow@users.noreply.github.com>
2022-12-03 22:25:07 -0800

    tested things

--- Main.py
+++ Main.py
@@ -28,8 +28,20 @@ PROJECT OUTLINE:
 Sources: 
 
 """
-import requests
-res = requests.get('https://herenow.com/results/#/races/21065/results')
-type(res)
-res.status_code == requests.codes.ok
-print(res.text[500:1000])
+import requests, bs4, time, requests_html
+from selenium import webdriver
+import pandas as pd
+import bs4
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from requests_html import HTMLSession
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome('C:\GitHub\IntroToProgramming2022\intoToProgrammingFinalProject\chromedriver_win32.zip\chromedriver')  
+driver.get('https://herenow.com/results/#/races/20899/results')
+time.sleep(60)
+element1 = driver.find_element(By.XPATH, '//*[@id="displayResultsView"]/section/ul/li[1]/div/div/table/tbody/tr[1]')
+
+print(element1.text)
+driver.quit
